b2ada8bc93bbe28a23b24e1ca1d618bea79836eb
Jose Salazar <znx3p0@gmail.com>
2021-03-29 18:20:27 -0700

    added default encryption

--- src/encryption/encryption.rs
+++ src/encryption/encryption.rs
@@ -3,8 +3,10 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait Encrypt {
-    async fn encrypt(input: Vec<u8>, key: &[u8]) -> Res<Vec<u8>>;
-    async fn decrypt(out: Vec<u8>, key: &[u8]) -> Res<Vec<u8>>
-    where
-        Self: Sized;
+    async fn encrypt(input: Vec<u8>, key: &[u8]) -> Res<Vec<u8>> {
+        Ok(input.into_iter().map(|s| s.rotate_right(s.count_ones() + *key.get(0).unwrap_or(&3) as u32)).collect())
+    }
+    async fn decrypt(out: Vec<u8>, key: &[u8]) -> Res<Vec<u8>> {
+        Ok(out.into_iter().map(|s| s.rotate_left(s.count_ones() + *key.get(0).unwrap_or(&3) as u32)).collect())
+    }
 }
